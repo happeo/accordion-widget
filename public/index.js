b518ec7445f80222e929611f7085998a6ec2855d
Gaurav Bhorkar <gaurav.bhorkar@happeo.com>
2022-12-02 09:21:21 +0530

    chore: WIP: add cypress config
    Signed-off-by: Gaurav Bhorkar <gaurav.bhorkar@happeo.com>

---
+++ public/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AccordionWidget from "../src/AccordionWidget";
+
+ReactDOM.render(<AccordionWidget id={"123"} editMode={true}/>, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import AccordionWidget from "./AccordionWidget";
 
 class happeoCustomReactWidget extends HTMLElement {
